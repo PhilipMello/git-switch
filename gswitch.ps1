bd8f233de672b38986442b5e3c80dc6408bba44f
Philip Mello <philipmello@live.com>
2024-10-28 10:20:09 +0000

    💫Update (gswitch.ps1 updated)

--- gswitch.ps1
+++ gswitch.ps1
@@ -1,55 +1,189 @@
-#about_Execution_Policies
-#https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.4
+# Author
+$author = "Philip Mello <@Microsoft>"
+# Version
+$version = "1.2"
+# License
+$license = "MIT"
+# Date
+$currentDate = (Get-Date).ToString("MMM dd, yyyy")
+# GitHub
+$github = "https://github.com/PhilipMello/git-switch"
 
-# To run git-switch.ps1, set: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-#
-# To remove run: Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+$script = "GitHub Account Switch"
+$description = "Multiple Github Accounts into one CLI"
 
-function Show-Menu {
-    param (
-        [string]$Title = 'Choose an Option'
-    )
+Write-Host @"
+        _____         _ _       _     
+       / ____|       (_) |     | |      $version
+  __ _| (_____      ___| |_ ___| |__    $currentDate
+ / _. |\___ \ \ /\ / / | __/ __| |_ \   $author
+| (_| |____) \ V  V /| | || (__| | | |  $license
+ \___ |_____/ \_/\_/ |_|\__\___|_| |_|
+  __/ |                               
+ |___/  $github                            
 
-    Clear-Host
-    Write-Host "================ $Title ================"
-    Write-Host "1: GitHub Account 1"
-    Write-Host "2: GitHub Account 2"
-    Write-Host "Q: Quit"
+# --------------------------------------------------------------
+# Script     : $script
+# Description: $description
+# --------------------------------------------------------------
+# How to use: Execute gswitch
+# Examples:
+# Manual: gswitch -h OR gswitch --h OR gswitch --help
+# Switch account using parameter: gswitch --account1
+# --------------------------------------------------------------
+"@
+
+function github-generate-sshkey {
+    Write-Host "Go to your GitHub Portal and add your SSH Key https://github.com/settings/ssh/new"
+
+    # Prompt user for the number of accounts to create
+    $numAccounts = Read-Host "How many GitHub accounts do you want to create (0-9)?"
+
+    # Validate input
+    if (-not ($numAccounts -match '^[0-9]+$') -or $numAccounts -lt 0 -or $numAccounts -gt 9) {
+        Write-Host "Invalid number. Please enter a number between 0 and 9."
+        exit 1
+    }
+
+    # Generate SSH keys for each account
+    for ($i = 0; $i -lt $numAccounts; $i++) {
+        $githubEmailAccount = Read-Host "Type your GitHub email account for account #$i"
+
+        # Basic email format validation
+        if ($githubEmailAccount -notmatch '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') {
+            Write-Host "Invalid email format. Please try again."
+            continue
+        }
+
+        ssh-keygen -t ed25519 -C "$githubEmailAccount" -f "$HOME/.ssh/id_ed25519_account_$i"
+        Write-Host "
+        +-----------------------------------------------------------+
+        | SSH key generated for: $githubEmailAccount
+        | Key saved as: $HOME/.ssh/id_ed25519_account_$i
+        +-----------------------------------------------------------+
+        "
+    }
 }
 
-# Prompt user to choose an option
-Show-Menu
-$selection = Read-Host "Select GitHub Account"
+function github-connection-test {
+    Write-Host "Github connection testing..."
+    ssh -T git@github.com
+}
 
-switch ($selection) {
-    '1' {
-        # Execute command to rename file 1
-        $gitHubAccount1 = "%userprofile%\.ssh\id_ed25519_account1"
-        $gitHubAccount1_pub = "%userprofile%\.ssh\id_ed25519_account1.pub"
-        $newFileName1 = "id_ed25519"
-        $newFileName1_pub = "id_ed25519.pub"
-        Rename-Item -Path $gitHubAccount1 -NewName $newFileName1
-        Rename-Item -Path $gitHubAccount1_pub -NewName $newFileName1_pub
-        Write-Host "Switched to GitHub Account 1"
+function github-set-account {
+    $githubSetAccountName = Read-Host "Enter your Github Account Name"
+    $githubSetAccountEmail = Read-Host "Enter your Github Account Email"
+    git config user.name "$githubSetAccountName"
+    git config user.email "$githubSetAccountEmail"
+}
+
+function github-account-switch {
+    Write-Host "You're logged in as:"
+    ssh -T git@github.com
+
+    $emails = @()
+    $files = @()
+
+    Get-ChildItem -Path ~/.ssh/id_* -File | ForEach-Object {
+        $content = Get-Content $_.FullName | Select-String -Pattern '[A-Za-z0-9._%+-]+@([A-Za-z0-9._%+-]+)?'
+        if ($content) {
+            $emails += $content.Matches.Value
+            $files += $_.Name
+        }
     }
-    '2' {
-        # Execute command to rename file 2
-        $gitHubAccount1 = "%userprofile%\.ssh\id_ed25519"
-        $gitHubAccount1_pub = "%userprofile%\.ssh\id_ed25519.pub"
-        $newFileName1 = "id_ed25519_account1"
-        $newFileName1_pub = "id_ed25519_account1.pub"
-        Rename-Item -Path $gitHubAccount1 -NewName $newFileName1
-        Rename-Item -Path $gitHubAccount1_pub -NewName $newFileName1_pub
-        Write-Host "Switched to GitHub Account 2"
+
+    if ($emails.Count -eq 0) {
+        Write-Host "No accounts found."
+        exit 1
     }
-    'q' {
-        Write-Host "Quitting..."
-        return
+
+    Write-Host "Select an email:"
+    for ($i = 0; $i -lt $emails.Count; $i++) {
+        Write-Host "$($i + 1). $($emails[$i])"
     }
-    default {
-        Write-Host "Invalid option. Please choose 1, 2, or Q."
+
+    $choice = Read-Host "Enter the number of your account"
+    if (-not ($choice -match '^[0-9]+$') -or $choice -lt 1 -or $choice -gt $emails.Count) {
+        Write-Host "Invalid choice. Exiting."
+        exit 1
+    }
+
+    $selectedEmail = $emails[$choice - 1]
+    $selectedFile = $files[$choice - 1]
+
+    Write-Host "Github Account $selectedEmail Selected!"
+    Copy-Item -Path ~/.ssh/$selectedFile -Destination ~/.ssh/id_ed25519.pub
+    ssh -T git@github.com
+}
+
+function github-fix-permissions {
+    Write-Host "Fixing SSH file permissions..."
+    icacls $HOME\.ssh /grant "$($env:UserName):(OI)(CI)F"
+    icacls $HOME\.ssh\id_* /inheritance:r /grant "$($env:UserName):F"
+    Write-Host "
+    +-----------------------------------------------------------+
+    | Files permissions have been fixed
+    +-----------------------------------------------------------+
+    "
+}
+
+function github-show-config {
+    Write-Host "Run gSwitch in git directory level"
+    git config --get-regexp "name|email"
+}
+
+function github-show-accounts {
+    Write-Host "Showing accounts in ~/.ssh"
+    Get-Content ~/.ssh/id_* | Select-String -Pattern '[A-Za-z0-9._%+-]+@([A-Za-z0-9._%+-]+)?' | ForEach-Object {
+        Write-Host "-----------------------------------------"
+        Write-Host "| Email: $($_.Matches[0].Value) |"
+        Write-Host "-----------------------------------------"
     }
 }
 
-# Display account changed message
-Write-Host "GitHub Account changed"
+function manual {
+    Write-Host "Generate 2 SSH Keys and rename GitHub account to id_ed25519_account1 and GitHub account 2 to id_ed25519_account2 in ~/.ssh"
+    Write-Host "Parameters:
+    --account1          Switch to GitHub Account 1
+    --account2          Switch to GitHub Account 2
+    "
+    exit 0
+}
+
+if ($args -contains "-h" -or $args -contains "--help" -or $args -contains "--h") {
+    manual
+} elseif ($args -contains "--account1") {
+    Copy-Item -Path ~/.ssh/id_ed25519_account1 -Destination ~/.ssh/id_ed25519
+    Copy-Item -Path ~/.ssh/id_ed25519_account1.pub -Destination ~/.ssh/id_ed25519.pub
+    Write-Host "Github Account #1 has been selected!"
+    ssh -T git@github.com
+    exit 0
+} elseif ($args -contains "--account2") {
+    Copy-Item -Path ~/.ssh/id_ed25519_account2 -Destination ~/.ssh/id_ed25519
+    Copy-Item -Path ~/.ssh/id_ed25519_account2.pub -Destination ~/.ssh/id_ed25519.pub
+    Write-Host "Github Account #2 has been selected!"
+    ssh -T git@github.com
+    exit 0
+}
+
+Write-Host "Choose an option:"
+Write-Host "1. Switch GitHub Account"
+Write-Host "2. Generate SSH Key"
+Write-Host "3. Test Github SSH connection"
+Write-Host "4. Set Github account"
+Write-Host "5. Fix SSH file permissions"
+Write-Host "6. Show current GitHub config"
+Write-Host "7. Show Account in: ~/.ssh"
+
+$option = Read-Host
+
+switch ($option) {
+    "1" { github-account-switch }
+    "2" { github-generate-sshkey }
+    "3" { github-connection-test }
+    "4" { github-set-account }
+    "5" { github-fix-permissions }
+    "6" { github-show-config }
+    "7" { github-show-accounts }
+    default { Write-Host "Invalid option" }
+}
